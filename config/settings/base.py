4fd6964592768107318014b6925df268d1261c75
Ashisane <ash.cpin@gmail.com>
2025-06-13 01:39:06 +0530

    Add Procfile and render.yaml for Render deployment

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,7 +79,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
