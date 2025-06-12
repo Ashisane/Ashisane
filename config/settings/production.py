d7eccacb605b6d58548e38049346c8dd560378dd
Ashisane <ash.cpin@gmail.com>
2025-06-13 02:18:21 +0530

    Fixed production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,8 +8,7 @@ DATABASES = {
         default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
-    ),
-    'ENGINE': 'django.db.backends.postgresql',
+    )
 }
 
 ALLOWED_HOSTS = ['https://ashisane.onrender.com', 'render.com']
